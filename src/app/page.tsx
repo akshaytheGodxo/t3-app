feefbe8b4301d214066db961fd1da0316ded7fb3
akshaytheGodxo <akshaysinghc230@gmail.com>
2024-12-29 23:33:28 +0530

    Update Next.js configuration, add TopNav component, and implement mock image display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,19 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
+
+const mockUrls = [
+  "https://utfs.io/f/6YYJmdTwczhibGncD31D81TxWFAtsHoVbS6YqPN5BgnwfLOy",
+  "https://utfs.io/f/6YYJmdTwczhipHSsIehh4NIvaJsZUt816zwVG05kjnXDplSM",
+  "https://utfs.io/f/6YYJmdTwczhi4O1APWJbs9mHT6BxfEhCRKSPvWrzA74MjupY",
+];
+
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
@@ -10,8 +23,16 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        hello
+      <main className="">
+        
+        <div className="flex flex-wrap gap-4">{
+          mockImages.map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} alt="image"/>
+            </div>
+          ))}
+        </div>
+          
       </main>
     </HydrateClient>
   );
